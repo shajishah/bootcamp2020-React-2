952b9cdd07cc5b37c13f14ec800a0c35e5a3df63
syedshajihaider <syedshajihaider@gmail.com>
2020-06-08 19:25:26 +0500

    main.yml added

--- src/Gate.js
+++ src/Gate.js
@@ -5,7 +5,7 @@ const Gate=({isOpen})=>(
     <>
     {isOpen == true ? <h2>Open</h2> : <h2>Closed</h2>}
 
-        <MediaCard body={"this is body "} imageUrl={"this is image URL"} title={"This is title"} />
+        <MediaCard body={"this is body "} imageUrl={"this is a image URL"} title={"This is title"} />
     </>
 )
 
