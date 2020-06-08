154bf3017c895899c89e7562fc3004193825ca49
syedshajihaider <syedshajihaider@gmail.com>
2020-06-08 19:29:27 +0500

    Update Gate.js

--- src/Gate.js
+++ src/Gate.js
@@ -3,7 +3,7 @@ import MediaCard from "./MediaCard";
 
 const Gate=({isOpen})=>(
     <>
-    {isOpen == true ? <h2>Open</h2> : <h2>Closed</h2>}
+    {isOpen === true ? <h2>Open</h2> : <h2>Closed</h2>}
 
         <MediaCard body={"this is body "} imageUrl={"this is a image URL"} title={"This is title"} />
     </>
